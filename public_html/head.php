e859048189d58c2972c4d27e3b44638c42641d5a
TimoLehnertz <timolehnertz1@gmail.com>
2021-04-04 14:26:44 +0200

    title

--- public_html/head.php
+++ public_html/head.php
@@ -21,7 +21,7 @@ if($loggedIn){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
-        <title>Inline data</title>
+        <title>Roller Results</title>
         <link rel="icon" type="image/png" href="/img/logo.png">
         <link rel="stylesheet" href="/styles/main.css">
         <!-- GOOGLE fonts -->
