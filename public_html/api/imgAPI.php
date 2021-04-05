2f50b259e586b7bcc530f58f2b3ea9290bfd061a
TimoLehnertz <timolehnertz1@gmail.com>
2021-04-05 13:47:29 +0200

    s

--- public_html/api/imgAPI.php
+++ public_html/api/imgAPI.php
@@ -55,7 +55,13 @@ function uploadImg($file, $prefix = ""){
     $nameNew = $prefix . uniqid('', true) . '.' . $ext;
     $dest = '../img/uploads/' . $nameNew;
     $succsess = move_uploaded_file($tmp, $dest);
-    resize_image($dest, 100, 100);
+    
+    /**
+     * resize
+     */
+    $image = resize_image($dest, 100, 100);
+    
+    
     if($succsess){
         return $nameNew;
     }
