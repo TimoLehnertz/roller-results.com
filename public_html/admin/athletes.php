31d0e98a97f136684993a1ecd939184addd588be
TimoLehnertz <timolehnertz1@gmail.com>
2021-04-05 13:23:34 +0200

    s

--- public_html/admin/athletes.php
+++ public_html/admin/athletes.php
@@ -39,9 +39,9 @@ if(isset($_POST["submit-changes"])){
     if(isset($_GET["idperson"])){
         $idperson = intval($_GET["idperson"]);
         $name = $_POST["firstname"]. "-" . $_POST["lastname"];
-        var_dump($_FILES);
-        exit();
-        if(isset($_FILES["image"])){
+        // var_dump($_FILES);
+        // exit();
+        if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])){
             // vardump($_FILES["image"]);
             // exit();
             $img = uploadImg($_FILES["image"], "athlete-$name-");
