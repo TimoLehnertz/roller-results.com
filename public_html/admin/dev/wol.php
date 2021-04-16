49665014ae7c8590ecf22012b4a10eae1cb59fb1
root <timolehnertz1@gmail.com>
2021-04-16 21:51:57 +0200

    raspi

--- public_html/admin/dev/wol.php
+++ public_html/admin/dev/wol.php
@@ -1,6 +1,7 @@
 <?php
 
-print shell_exec("sudo etherwake E0:3F:49:A2:11:A9")."<hr>"
+echo shell_exec("sudo etherwake E0:3F:49:A2:11:A9")."<hr>"
+#echo shell_exec("echo hallo")."<hr>"
 
 ?>
 Waking up
