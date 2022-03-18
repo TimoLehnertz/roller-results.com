82bfc35bc78b86002b2eed8aa8f36c9d2630787d
TimoLehnertz <timolehnertz1@gmail.com>
2022-03-18 14:27:04 +0000

    s

--- public_html/head.php
+++ public_html/head.php
@@ -34,7 +34,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
         <meta property="og:type" content="profile" />
 
         <link rel="icon" type="image/png" href="/img/logo.PNG">
-        <link rel="stylesheet" href="/styles/main.css">
+        <link rel="stylesheet" href="/styles/main.css?v=1.1">
         <!-- GOOGLE fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap" rel="stylesheet">
@@ -53,7 +53,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.bundle.js"></script>
 
         <script src="/js/ajaxAPI.js"></script>
-        <script src="/js/lib.js"></script>
+        <script src="/js/lib.js?v=1.1"></script>
         <script src="/js/ui.js"></script>
         <script src="/js/interface.js"></script>
         <script src="/js/html2canvas/html2canvas.js" defer></script>
