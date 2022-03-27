9ba167c291cddb54c0b657e611d8585a9a382641
TimoLehnertz <timolehnertz1@gmail.com>
2022-03-27 04:41:19 +0200

    s

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -226,13 +226,14 @@ if(!isset($NO_GET_API)){
         }
     }
     else if(isset($_GET["uploadResults"])){
-        dbExecute("INSERT INTO TbLaserResults(remark) VALUE(?);", "s", file_get_contents('php://input'));
-        $data = json_decode(file_get_contents('php://input'), true);
+        // dbExecute("INSERT INTO TbLaserResults(remark) VALUE(?);", "s", file_get_contents('php://input'));
+        // $data = json_decode(file_get_contents('php://input'), true);
+        $data = json_decode($_GET["data"], true);
         if(!isset($_GET["lname"]) || !isset($data["a"]) || !isset($data["d"]) || !isset($data["l"]) || !isset($_GET["user"])) {
             echo "invalid";
             // exit(0);
         }
-        dbExecute("INSERT INTO TbLaserResults(remark) VALUE('moin');");
+        // dbExecute("INSERT INTO TbLaserResults(remark) VALUE('moin');");
         insertLaserResult($data, $_GET["user"], $_GET["lname"]);
     }
     else if(isset($_GET["getteamAdvantageDetails"])){
