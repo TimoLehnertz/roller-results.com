6d09c9b3ca4a789a075a3cc1b566600d9b95a5bd
TimoLehnertz <timolehnertz1@gmail.com>
2022-08-22 21:07:29 +0200

    s

--- public_html/head.php
+++ public_html/head.php
@@ -34,7 +34,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
         <meta property="og:type" content="profile" />
 
         <link rel="icon" type="image/png" href="/img/logo/rr.png">
-        <link rel="stylesheet" href="/styles/main.css?v=1.3">
+        <link rel="stylesheet" href="/styles/main.css?v=1.4">
         <!-- GOOGLE fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap" rel="stylesheet">
@@ -52,10 +52,10 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
         <!-- chart.js -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.bundle.js"></script>
 
-        <script src="/js/ajaxAPI.js?v=1.0"></script>
-        <script src="/js/lib.js?v=1.2"></script>
-        <script src="/js/ui.js?v=1.0"></script>
-        <script src="/js/interface.js?v=1.0"></script>
+        <script src="/js/ajaxAPI.js?v=1.1"></script>
+        <script src="/js/lib.js?v=1.3"></script>
+        <script src="/js/ui.js?v=1.2"></script>
+        <script src="/js/interface.js?v=1.1"></script>
         <script src="/js/html2canvas/html2canvas.js" defer></script>
         <script>
             <?php echo "const phpUId = '".uniqid($_SERVER["SERVER_NAME"], true)."';";?>
