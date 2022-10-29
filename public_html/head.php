5ddf0a162f2a5a105daaa4926b64b1ec98000495
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2022-10-29 14:34:19 +0200

    s

--- public_html/head.php
+++ public_html/head.php
@@ -44,7 +44,7 @@ $actual_link = htmlentities((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === '
         <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
 
         <link rel="icon" type="image/png" href="/img/logo/rr.png">
-        <link rel="stylesheet" href="/styles/main.css?v=1.4">
+        <link rel="stylesheet" href="/styles/main.css?v=1.5">
         <!-- GOOGLE fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap" rel="stylesheet">
