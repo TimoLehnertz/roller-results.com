a6da43056182214166f857986040da4e63bf43b3
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2022-11-26 14:07:04 +0100

    s

--- public_html/calendar/index.php
+++ public_html/calendar/index.php
@@ -9,10 +9,16 @@ echoRandWallpaper();
 <main class="main competitions-page">
 <iframe id="open-web-calendar" 
     style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
-    src="https://open-web-calendar.herokuapp.com/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fq1pj0ferd2biqsbj3i105rjhuo%2540group.calendar.google.com%2Fpublic%2Fbasic.ics&amp;language=en"
+    src="https://open-web-calendar.herokuapp.com/calendar.html?url=https://calendar.google.com/calendar/u/0?cid=NTdnNXBkbzUwdHVkamRvcjhscWRkNDFkYWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
     sandbox="allow-scripts allow-same-origin allow-top-navigation"
     allowTransparency="true" scrolling="no" 
     frameborder="0" height="600px" width="100%"></iframe>
+    <!-- <iframe id="open-web-calendar" 
+    style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
+    src="https://open-web-calendar.herokuapp.com/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fq1pj0ferd2biqsbj3i105rjhuo%2540group.calendar.google.com%2Fpublic%2Fbasic.ics&amp;language=en"
+    sandbox="allow-scripts allow-same-origin allow-top-navigation"
+    allowTransparency="true" scrolling="no" 
+    frameborder="0" height="600px" width="100%"></iframe> -->
 </main>
 <?php
 include_once "../footer.php";
