8361c6a962d5b3a7d3e718d4a1a8e19d9c9c0d18
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-03-27 15:48:27 +0100

    added cors

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -800,12 +800,38 @@ if(!isset($NO_GET_API) || $NO_GET_API === false) {
     } else if(isset($_GET["getclubAthletes"])) {
         echo json_encode(getAthletesByClub($_GET["getclubAthletes"]));
     } else if(isset($_GET["getPascalPlaces"])) {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
-
+        cors();
         echo json_encode(getPascalMemberships());
     }
 }
+
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+    
+    echo "You have CORS!";
+}
+
 
 function rollerTimingNameExists(string $name): bool {
     return sizeof(query("SELECT idRollerTiming FROM TbRollerTiming WHERE `session`=? LIMIT 1", "s", $name)) > 0;
