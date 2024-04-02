b389650c39764e4c6bb82f25102f93e486a15022
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-04-02 17:53:23 +0200

    s

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -806,7 +806,7 @@ if(!isset($NO_GET_API) || $NO_GET_API === false) {
         $path = $_SERVER["DOCUMENT_ROOT"]."/../logs";
         if (!file_exists($path)) {
             // dir doesn't exist, make it
-            echo mkdir($path);
+            var_dump(mkdir($path));
             echo "mkdir ";
           }
         echo $path;
